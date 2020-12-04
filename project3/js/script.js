e4f7841706d384138d17d004477a72529af92a54
Nur Alam <56593129+Nuralam30@users.noreply.github.com>
2020-12-04 23:22:21 +0600

    psd to html

---
+++ project3/js/script.js
@@ -0,0 +1,12 @@
+
+// const nav = document.querySelector(".navbar");
+// const sbtn = document.querySelector(".checkbtn");
+// const rbtn = document.querySelector(".removebtn");
+
+// sbtn.addEventListener("click", function(){
+// 	nav.classList.add("active")
+// })
+
+// rbtn.addEventListener("click", function(){
+// 	nav.classList.remove("active")
+// })
